fe6bb63c0619fe309b3deaad7e96b369410e6b75
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-04 17:33:20 +0530

    unique Function

---
+++ 2_Numpy/3_Numpy_Tricks/4_np_unique_.py
@@ -0,0 +1,10 @@
+# Unique Methode Se Haam Array Se Unique Nikal ke Deta hai 
+# Ye jaab Use Hota Hia Jab Bahut sare data hia But hiai Etc 
+# Agr mai Eska Example Du :
+# Soppose karo Ki Tum Ek data Collet kar rahe ho Ki kisko Kisko 5 subject diye gaye hai Unme Se pasand Hia 
+# TOh Sabne Appna appna Data Dala hai Ok ok Esme unique Se haam Unique find kar lenge 
+# Bahut  Use Ful unction Hia 
+
+import numpy as np
+a = np.array([1,1,1,1,1,2,1,1,3])
+print(np.unique(a))     # [1 2 3]
