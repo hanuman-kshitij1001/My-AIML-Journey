cd20a35089a907c0d29ffcfd37a15b99573ac5ef
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-04 21:29:29 +0530

    delete_function

---
+++ 2_Numpy/3_Numpy_Tricks/15_np_delete_.py
@@ -0,0 +1,9 @@
+# Delete A Sub Array Along The Mention Axis Delete the Main Array 
+
+
+import numpy as np
+a = np.array([11, 53, 28, 50, 38, 37, 94, 92, 5, 30, 68, 9, 78, 2, 21])
+b = np.delete(a, 0) # 0 is Index
+print(b)
+
+# Not more Use full Sayad Kahi kaam aye 
