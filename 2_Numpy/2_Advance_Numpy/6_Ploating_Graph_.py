1a00aaee1b0d9a0ae866cf3c223904995f7c06d0
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-04 16:57:09 +0530

    Updates Folder/Files

--- 2_Numpy/2_Advance_Numpy/6_Ploating_Graph_.py
+++ 2_Numpy/2_Advance_Numpy/6_Ploating_Graph_.py
@@ -32,3 +32,16 @@ x = np.linspace(-10, 10, 100)
 y = 1/(1+np.exp(-x))
 plt.plot(x,y)
 plt.show()
+
+
+# Ye Sir Ne Nahi Karaya hai Ye Meri Chul Thi Maine Chat Gpt Se kara hai 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 30, 40, 50]
+z = [100, 200, 300, 400, 500]
+ax.scatter(x, y, z)
+plt.show()
