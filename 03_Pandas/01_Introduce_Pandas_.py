cd08367d27fd895c6cb7c86627f1ae6c4064e3d6
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-05 14:16:30 +0530

    Introduce pandas series

---
+++ 03_Pandas/01_Introduce_Pandas_.py
@@ -0,0 +1,11 @@
+# What is Pandas:? >>
+# Pandas is a fast, powerful, flexible and easy to use open source data analysis and manipulation tool, built on top of the Python programming language.
+# Kissi Bhi Type Ke PRidiction Model Me Apko Sanse pahle data ko analyis karn apadta hai apko samjhna padhta hai ki data me chal kya raha hia 
+# Import baat data ko analysis karne ke liye pandas jaise import tool chahiye hi honga 
+# Toh Yaha Se Thoda Bahut ye idea toh lag hi raha hai ki Prediction Model me pandas bahut import hai 
+# Pandas is a Tool Which Is Usse Any Kind Of Ananlysis And munupulation in data , Daata Ko Pura khol ke rakh deta hai 
+# Pandas is So Power Full ye Kissi bhi type Ke Data pe Kaam Kar Sakta hai 
+
+# Oandash Me Bhi Bahut se type ke object hote hia Unme 2 Toh Bahut famous hai 
+# 1- Pandas Series
+# 2- Data Frame
---
+++ 03_Pandas/02_Pandas_Series/01_Introduce_pandas_sereis_.py
@@ -0,0 +1,4 @@
+# Pandas Series
+# A Pandas Series is like a column in a table. It is a 1-D array holding data of any type.
+# Ye EK 1-D Data hai jaha Pe aap kisis Bhi types ka data types store kar sakte ho 
+# Agr mai Aur Simple Word me bolu Toh Esse Bol sakte hia kia Agr apke passs ek table hia aut aap uske 1 col ya row (sayad row nahi) ko series bula sakte ho 
