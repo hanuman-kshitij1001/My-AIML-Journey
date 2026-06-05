9d90c7db118f873930edc760831da3a53b2f9ccd
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-05 14:35:12 +0530

    How To Import pandas

---
+++ 03_Pandas/02_Pandas_Series/02_How_to_import_pandas_.py
@@ -0,0 +1,13 @@
+# Agr aap Pahili baar pandas chalne ja rahe to Toh apko Likhna Hong Ki 
+# Step 1: Pandas install karo (agar pehle se nahi hai)
+# Step 2: Terminal / CMD me likho:
+# pip install pandas
+# Step 3: Python file me import kaise kare
+# import pandas as pd
+# Yaha pd ek alias (short name) hai, jisse code easy ho jata hai.
+
+# Main Step : import pandas as pd
+
+# Note Pandas Basiclly numpy Ke Upper bana Hai It Is A good Idea Ki pandas Ko Import Kare aur numpy ko bhi kare 
+# Although Jaruri nahi hai par kabhi na kabhi kaam ayega 
+
