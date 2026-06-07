1d2f648b18f91ef0c43c1fb55bb15b9158b73e9a
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-07 16:17:33 +0530

    sort_values

--- 03_Pandas/04_Important_DF_Method/01_More_Imp_Functions.py
+++ 03_Pandas/04_Important_DF_Method/01_More_Imp_Functions.py
@@ -62,6 +62,39 @@ print(b)
 #Q3 : # how many matches each team has played
 c = (ipl['Team2'].value_counts() + ipl['Team1'].value_counts()).sort_values(ascending=False)
 (ipl['Team2'].value_counts() + ipl['Team1'].value_counts()).sort_values(ascending=False).plot(kind='pie')
-print(c)
-plt.show()
+# print(c)
+# plt.show()
 
+
+
+#Method 2: sort_value ( series aur df dono pe applicable hai)
+x = pd.Series([12,14,1,56,89])
+print(x.sort_values(ascending=False))
+
+# Chalo esse Data Frame Pe kaam karte hai chalo haam movies wala data uthate hai 
+print(movies.head(4))
+
+# Ab Haam Dekhte Hai Ki kaise Espe Sort values apply karte hai 
+# Sabse pahele mai Single col pe chata hun phir multiple col pe bhi kaam karenge 
+# Mujhe ye chaliye Mera Data Solve ho Jaye Col Ke hisab se 
+print(movies.sort_values(['title_x']))
+# THis Is The way To Sort data frame On the basis of 1 col 
+
+students = pd.DataFrame(
+    {
+        'name':['nitish','ankit','rupesh',np.nan,'mrityunjay',np.nan,'rishabh',np.nan,'aditya',np.nan],
+        'college':['bit','iit','vit',np.nan,np.nan,'vlsi','ssit',np.nan,np.nan,'git'],
+        'branch':['eee','it','cse',np.nan,'me','ce','civ','cse','bio',np.nan],
+        'cgpa':[6.66,8.25,6.41,np.nan,5.6,9.0,7.4,10,7.4,np.nan],
+        'package':[4,5,6,np.nan,6,7,8,9,np.nan,np.nan]
+
+    }
+)
+print(students.sort_values('name'))
+print(students.sort_values('name',na_position='first',ascending=False)) # yaha Pe na wali values Sabse top ho jayegi
+
+# ab Baat karte hai ki Multiple col se sorting karna hai toh kaise karenge 
+# yaha Pe of release ka col hai tile ka col mai es dataset ko essa karna chahta hun ki soring hoo year ke hisab se 
+# Phir mai kya karna chata hun ki uss movies me ghus ke sari movies ko alpha sort karunga 
+
+print(movies.sort_values(['year_of_release','title_x'],ascending=[True,False]))
