ee172ad88fc9f2f602782248f274a64108ac60c2
Kshitij Tiwari <kshitijji001@gmail.com>
2026-06-07 15:55:29 +0530

    value_counts/sort_values

--- 03_Pandas/04_Important_DF_Method/01_More_Imp_Functions.py
+++ 03_Pandas/04_Important_DF_Method/01_More_Imp_Functions.py
@@ -21,7 +21,7 @@
 # Each one of them is Important 
 # Agr Ye 20+ function Acche se use karna a gaye toh app data set bahut acche se usse aur maupulate kar paoge 
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 marks = pd.DataFrame([
@@ -31,7 +31,9 @@ marks = pd.DataFrame([
     [80,70,14],
     [80,70,14]
 ],columns=['iq','marks','package'])
-
+ipl = pd.read_csv("03_Pandas/04_Important_DF_Method/02_Data_set_IPL_.csv")
+movies = pd.read_csv("03_Pandas/04_Important_DF_Method/03_Data_Set_movies_.csv")
+health = pd.read_csv("03_Pandas/04_Important_DF_Method/04_DataSet_Health.csv")
 
 # Method 1: value_counts(series and dataframe)
 # Har Unique Item ki Freq Ka Count nikal ke deta hai 
@@ -39,3 +41,27 @@ marks = pd.DataFrame([
 a = marks.value_counts()  # Ye Pure Rows Ka Freq Count nikal Raha hai 
                           # Ye bahut usse full nahi rahta hai Pure data frame p elagta hia esliye But hn ye series me Kaam KArta hai 
 print(a)
+
+
+#Q1 : # find which player has won most potm -> in finals and qualifiers
+      # Hame Uss Player ka Name Niklna JIsne Ipl Ke Final ya Semi-final usme sabse jada man of the matach award jite hai 
+
+a = ipl[~ipl['MatchNumber'].str.isdigit()]['Player_of_Match'].value_counts()
+print(a)
+
+#Q2 Toss Jitne Ke baad Team Bating Karti ya Balling karti hai aur fhir espe char banna hai
+# Yaha Ek Col hai Toss Dicision Jo ye Bata raha hai ki Team Jitne ke baad balling kar rahi hia ye bating 
+# Chalo mai toss dission wale value pe value count laga dunga ye automatic mujhe bat denga kitni baar batinga kari hai 
+# aur kitni balling 
+b = ipl['TossDecision'].value_counts().plot(kind='pie')
+
+print(b)
+# plt.show()
+
+
+#Q3 : # how many matches each team has played
+c = (ipl['Team2'].value_counts() + ipl['Team1'].value_counts()).sort_values(ascending=False)
+(ipl['Team2'].value_counts() + ipl['Team1'].value_counts()).sort_values(ascending=False).plot(kind='pie')
+print(c)
+plt.show()
+
