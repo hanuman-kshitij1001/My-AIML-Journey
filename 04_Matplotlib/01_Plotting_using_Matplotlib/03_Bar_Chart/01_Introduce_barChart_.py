9018f6978d93dd75a9ed5dce7bb7d2e96e35aea2
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-11 15:09:07 +0530

    Introduce Bar Graph

---
+++ 04_Matplotlib/01_Plotting_using_Matplotlib/03_Bar_Chart/01_Introduce_barChart_.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+#1: DataSet
+batsman = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/01_batsman_season_record.csv")
+
+#2: Data set 
+batter = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/02_batter.csv")
+
+#3: Dataset:
+big_array = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/03_big-array.npy")
+
+#4: DataSet
+four_sixes  = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/04_fours-sixes.csv")
+
+#5: DataSet
+gayle = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/05_gayle-175.csv")
+
+#6: DataSet
+sharma_k = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/06_sharma-kohli.csv")
+
+#7: DataSet
+vk = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/07_vk.csv")
+
+
+# THis Is our 3rd Types 
+# yaha X axis pe Aap Koi Cat... colomn rakhte ho aur y axis pe aap Ko Agregate perform karte ho 
+
+# Key Points 
+# - Bivariate Analysis
+# - Numerical vs Categorical
+# - Use case - Aggregate analysis of groups
