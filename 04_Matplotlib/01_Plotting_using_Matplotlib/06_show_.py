efbb72791ffcf4e81d5b46d0d91e05cee2405c3a
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-11 14:11:41 +0530

    plot.Show function

---
+++ 04_Matplotlib/01_Plotting_using_Matplotlib/06_show_.py
@@ -0,0 +1,12 @@
+# Show Kya hai : 
+# Show kya karta hai ki 
+# Jo graph tumne banaya hai usko screen par display karna. haam show ka use karte hai 
+# plt .show() use nahi karoge toh graph plot nahi honge
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.plot([1,2,3],[10,20,30])
+plt.show()
