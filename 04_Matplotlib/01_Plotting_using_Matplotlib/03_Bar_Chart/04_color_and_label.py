bea6a31acd34abc879a8216ddff59af0d53cb35c
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-11 15:24:37 +0530

    lable And color

--- 04_Matplotlib/01_Plotting_using_Matplotlib/03_Bar_Chart/04_color_and_label.py
+++ 04_Matplotlib/01_Plotting_using_Matplotlib/03_Bar_Chart/04_color_and_label.py
@@ -6,11 +6,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #1: DataSet
-batsman = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/01_batsman_season_record.csv")
+df = batsman = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/01_batsman_season_record.csv")
 
 
-children = [10,20,40,10,30]
-colors = ['red','blue','green','yellow','pink']
+plt.bar(np.arange(df.shape[0]) - 0.2,df['2015'],width=0.2,color='orange')
+plt.bar(np.arange(df.shape[0]),df['2016'],width=0.2,color='red')
+plt.bar(np.arange(df.shape[0]) + 0.2,df['2017'],width=0.2,color='blue')
+
+plt.xticks(np.arange(df.shape[0]), df['batsman'])
+
+plt.show()
 
 
+print(np.arange(df.shape[0]))
 
