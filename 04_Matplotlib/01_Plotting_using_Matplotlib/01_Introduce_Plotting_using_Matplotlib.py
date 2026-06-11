35be8399d27b5e227daf479ed7970a4a081b60fd
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-11 12:04:29 +0530

    Introduce Matplotlib

--- 04_Matplotlib/01_Plotting_using_Matplotlib/01_Introduce_Plotting_using_Matplotlib.py
+++ 04_Matplotlib/01_Plotting_using_Matplotlib/01_Introduce_Plotting_using_Matplotlib.py
@@ -46,3 +46,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import seaborn as sns
+
+#1: DataSet
+batsman = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/01_batsman_season_record.csv")
+
+#2: Data set 
+batter = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/02_batter.csv")
+
+#3: Dataset:
+big_array = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/03_big-array.npy")
+
+#4: DataSet
+four_sixes  = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/04_fours-sixes.csv")
+
+#5: DataSet
+gayle = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/05_gayle-175.csv")
+
+#6: DataSet
+sharma_k = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/06_sharma-kohli.csv")
+
+#7: DataSet
+vk = pd.read_csv("04_Matplotlib/01_Plotting_using_Matplotlib/My_Data_Set/07_vk.csv")
