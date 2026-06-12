2120b57fa6611478199c4e2253e122896a656aa9
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-12 23:23:40 +0530

    Introduce Seaborn

---
+++ 05_Plotting_Using_Seaborn/01_Introduce_Seaborn.py
@@ -0,0 +1,34 @@
+# Why Seaborn?
+# its provides you a layer of abstraction hence simpler to use ..>> Matlb Kaam line kaam Karne ka Tarika 
+# better aesthetics ..>> Matlb jo apke graph ploat hote hia wo jada acche aur sunder bante hia jada design nahi karni padhti hai 
+# more graphs included...>> Kuch wo sare kaam bhi yaha mill jate hai jaha kahi nahi milte hai 
+# Comapany maximum yahi use karti hai 
+
+# Seaborn Roadmap
+
+# Mai Apko Road Map Deta hun agr apko seaborn sikhna hia toh kya kya sikhna chahiye 
+# Ek Confusion rahta hai longon ko ki seaborn me ap notice karoge ki do function kabhi kabhi same graph dete hai unka name alag honga but wo same hi graph display karenge 
+# Ye ek Confusion hai na ki bhai do alag name hia but dono graph toh same hi ploat kar rahe hai na Kyu Confusion that is ?
+# Wahi mai Apko Pahele CLear kar de raha hun 
+# Actually Hota kya hai ki seborn me do tarah ke graph hote hai ya fuction hote hia 
+
+# Types of Functions
+
+# >> Figure Level         :
+# >> Axis Level           :
+
+# >> Main Classification  : ye sare seaborn ke classi hai 
+# >> Relational Plot      : 1 -Sctter plot :  esme ata hai scatter plot ata hai   2 -Line plot:  apke x pe gerally time yer, monts kuch bhi jata aur y axis pe stocks etc wagera hota hai 
+# >> Distribution Plot    : jab aap ek single col ke uper  ploating karna chahte ho toh ye kaam ata hai eske andhr graphs ate hia 1- histograph , 2- KDE plot(Esse Age Chal Ke bahut kaam karoge)  3- Rug plot 
+# >> Categorical Plot     : Jo cato .. data ke uppper aap graph bana rahe hote ho >> Eske Andhr ane wale graph 1- bar, 2- count, 3- Box Plot, 4- violin, 5-swam plot  
+# >> Regression Plot      : Ye Ml Me kaam ata hai Ml me Ek Algo hota hia that is called lenaer regration  
+# >> Matrix Plot          : Ye grd ke upper use kiye jate hia
+# >> Multiplots           : esme apko do graph padhne hai 1- Point graph , 2- pair graph 
+
+# Ye apka Road maap hai Etna haame seaborn me padhna hota hai 
+# This API refence of seaborn eske upper seaborn me kuch nahi hai 
+#https://www.google.com/url?q=https%3A%2F%2Fseaborn.pydata.org%2Fapi.html
+
+# Seborn pe log jada Focus karte hai Kyu Ki Syntax me kaam Likhna Padhta hai 
+# Note Seaborn Kyu Padhe haam Why : 
+# Chalo Batate hai REson smjho bhai its provide
