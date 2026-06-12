3d26dd3bea6ac729298ed268d4da54a520d177b1
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-12 08:38:37 +0530

    Advance matplotlib

---
+++ 04_Matplotlib/02_Advance_Matplotlib/All_dataSet/01_Introduction_About_Ad_matp.py
@@ -0,0 +1,18 @@
+# About Todays Class 
+# Yaha Start me Haam Log Suru me Kuch Tricks dekhnge theek hai 
+# Fhir haam log Ek Topic padhenge that is Sub ploat 
+# Uske Baad Ham long New Types Ke Graph Padhege Specially 3D 
+# Fhir ek heat maap hota hai wo bhi padhenge 
+# Uske baad haam Thoda Piche Jayenge Aur pandas ka plot ko study karenge 
+
+# Step 1: Sari lib Import kar leni hia 
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+iris = pd.read_csv("04_Matplotlib/02_Advance_Matplotlib/All_dataSet/01_iris.csv")
+batter = pd.read_csv("04_Matplotlib/02_Advance_Matplotlib/All_dataSet/02_batter.csv")
+batsman_session = pd.read_csv("04_Matplotlib/02_Advance_Matplotlib/All_dataSet/03_batsman_season_record.csv")
+ipl = pd.read_csv("04_Matplotlib/02_Advance_Matplotlib/All_dataSet/04_IPL_Ball_by_Ball_2008_2022.csv")
+
