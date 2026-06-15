d0b5f6b80bd6f48076385f3c1a92276ae9b194dc
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-15 19:48:14 +0530

    Introduce Import pandas

---
+++ 06_Data Gathering_Analysis/03_Pandas_Exports/01_Intro_import.py
@@ -0,0 +1,26 @@
+# Pandas mein export ka matlab hota hai
+# DataFrame ke data ko kisi file mein save karna.
+
+# Import:
+# File → DataFrame
+
+# Export:
+# DataFrame → File
+
+# Data Analysis mein cleaning aur processing ke baad
+# hum data ko dobara CSV, Excel, JSON ya SQL Database mein save karte hain.
+
+# Pandas kai formats mein data export kar sakta hai:
+# 1. CSV
+# 2. Excel
+# 3. JSON
+# 4. SQL Database
+
+# Exporting ke liye Pandas ke paas alag-alag functions hote hain:
+# to_csv()
+# to_excel()
+# to_json()
+# to_sql()
+# to_html()
+
+#Note jitne bhi to wale functions hai yahi pandas ke import wale function hote hai 
