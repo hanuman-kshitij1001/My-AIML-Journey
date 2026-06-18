f38996550182078d8c27c500cd72fa6a7ec3b052
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-18 16:04:51 +0530

    Export csv from pandas

---
+++ 06_Data Gathering_Analysis/03_Pandas_Exports/02_to_csv_.py
@@ -0,0 +1,12 @@
+1#. Exporting To CSV
+# Sabse common export format.
+# Basic Syntax: df.to_csv("output.csv")
+
+# Ex:
+import pandas as pd
+df = pd.DataFrame({
+    'Name':['Kshitij','Rahul'],
+    'Marks':[85,90]
+})
+df.to_csv("students.csv")
+# Ye ek nayi file bana dega:  students.csv  se 
