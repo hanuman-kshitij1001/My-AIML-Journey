bc9850bed292578624e8f4e931176570ccd560c1
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-21 17:14:51 +0530

    Understanding How to Write The Information From Data

---
+++ 07_Data_Assessing_nd_Cleaning/02_Data_Accessing/07_Start_manual_ass.py
@@ -0,0 +1,53 @@
+# ab Yaha Pe Ess Excell ko Dekhnge aur apne observation ke Likhte hai 
+
+# ab Esse Mai Do Part Me Divide kAr raha hun 
+
+
+# Ye Analysis hai for "patients Table"
+#1 Dirty Data :
+#  - patient-Id - 9 has misspelled name 'Dsvid' insted of 'David'
+#  - state col- sometimes contains full name and some time abbrivieation
+#  - zip code col- has entries with 4 digits insted of 5 digits
+#  - In Contact col there is Strictually problem 
+
+#2 Messy Data :
+#  - In Contact col there is Strictually problem  cont col caontain both phone and email
+
+
+# # Ye Analysis hai for " treatment Table"
+#1 Dirty Data :
+#  - given_name and surname col is is all lower case `consistency` 
+#  - remove u from Auralin and Novadra cols `validity`
+#  - '-' in novadra and Auralin col treated as nan `validity`
+#  - missing values in hba1c_change col `completion`
+
+#2 Messy Data :
+#  -Auralin and Novadra col should be split into 2 cols start and end dose 
+# 
+
+
+# # Ye Analysis hai for " treatment_Cut  Table  ki "
+#1 Dirty Data :
+#  - 
+#  -
+# Yaha Dubra nahi likhunga treatment wali Hi same problem hia esme theek hai na 
+#2 Messy Data :
+#  - 
+#  - 
+
+
+# # Ye Analysis hai for " adverse_reactions  Table  ki "
+#1 Dirty Data :
+#  - Name as an given all in lower case
+#  -
+
+#2 Messy Data :
+#  - 
+#  - 
+
+#Understaing : col by col scann karn achalu karo 
+
+
+
+
+
