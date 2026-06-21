851dc7ddd3da6c9e834eaa4b5fa930df0983c80a
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-21 15:48:19 +0530

    Types of assessment Styles

---
+++ 07_Data_Assessing_nd_Cleaning/02_Data_Accessing/05_Types_of_Asse.py
@@ -0,0 +1,17 @@
+# Types of Assessment
+
+#There are 2 types of assessment styles
+#1-  Manual/Visual - 
+#            Looking through the data manually in google sheets
+#2-  Programmatic -
+#            By using pandas functions such as info(), describe() or sample()
+
+# Steps in Assessment
+
+# There are 2 steps involved in Assessment
+
+# 1. Discover : haame Usse search karenge 
+# Identify data quality and tidiness issues.
+
+# 2. Document : Fhri Usse yaha likh lenge 
+# Record all discovered issues before cleaning the data.
