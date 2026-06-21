efe4df24cb674100e2c9ef1a579fdbd131dd6552
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-21 13:27:28 +0530

    Data Assesment & Data Cleaning

---
+++ 07_Data_Assessing_nd_Cleaning/02_Introduce_.py
@@ -0,0 +1,33 @@
+# abhi Tak Hamne Data Analylist Process padha 
+# Basicalay Esme 5 Steps hote hai  Neche Diya gaya hai 
+# Data Analysis Process
+# 1-Asking Questions
+# 2-Data Wrangling
+#   a. Gathering Data - 
+#           i. CSV files - 
+#           ii. APIs - 
+#           iii. Web Scraping - 
+#           iv. Databases 
+#   b. Assessing Data
+#   c. Cleaning Data
+# 3-Exploratory Data Analysis  # ye EDA ke Sath Padhnege
+# 4-Drawing Conclusion         # EdA ke Sath
+# 5-Comunicating Results       # # EdA ke Sath
+
+
+# Summary of today's session
+# 1- You have to become Sherlock(USke adhr ghus ke Dekhna)
+# 2 -Will try to create a framework
+# 3- Frameworks may vary
+# 4 -The goal is to give you idea
+
+# ajj Ka jo session hia wo Shirf Esnpe hi hia 
+
+# b. Assessing Data
+# c. Cleaning Data
+
+# Data me Kya hai Kya Gadbad Hai Aur usse Clean Kaise karte hai Ye Sab Aaj haam yaha Sikhne Wale Hai 
+# haam Search Karenge Ki Gadh baad Kya hai aur Usse sahi karenge Fhir 
+# Note : Agr Tumhare Pass Ek Accha Frame work nahi honga Ek accha sochne ka Traika nahi honga toh aap Data Cleaning sahi se nahi kar paoge 
+# Sabse Jaruri Ye hai Ki agr apke pass data Set hai Toh Kya Steps honge Esse Clean Karne ke Wo Sab Aap Sikhonge Theek 
+# Frame Badal bhi jate hai .. NOte Frame Work == Sochne ka Tarika For cleanning 
