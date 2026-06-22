0c3967e87efbd8369427f53527127b490550b04a
Kshitij-tiwari <kshitijji001@gmail.com>
2026-06-22 16:36:43 +0530

    Step for data cleanning

---
+++ 07_Data_Assessing_nd_Cleaning/03_DataCleaning_/01_BasicBoforeDataCleanning_.py
@@ -0,0 +1,24 @@
+# Note - Assessing Data is an Iterative Process
+
+# Data Quality Dimensions
+#1-  Completeness -> is data missing?
+#2-  Validity -> is data invalid -> negative height -> duplicate patient id
+#3-  Accuracy -> data is valid but not accurate -> weight -> 1kg
+#4-  Consistency -> both valid and accurate but written differently -> New Youk and NY
+
+
+# Order of severity    ...>    Completeness <- Validity <- Accuracy <- Consistency
+
+# Data Cleaning Order
+#1-   Quality -> Completeness
+#2-   Tidiness
+#3-   Quality -> Validity
+#4-   Quality -> Accuracy
+#5-   Quality -> Consistency
+
+#Steps involved in Data cleaning
+#. Define
+#. Code
+#. Test
+
+# Note : Always make sure to create a copy of your pandas dataframe before you start the cleaning process
